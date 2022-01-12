d85dd906a1c11c80451936395bc69546855aa9b7
Jundi <indana6969@gmail.com>
2022-01-12 19:08:23 +0700

    adding react-leaflet maps on listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 
+// react leaflet
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+
 // firebase
 import { getDoc, doc } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
@@ -27,7 +30,6 @@ function Listing() {
             const docSnap = await getDoc(docRef)
 
             if (docSnap.exists()) {
-                console.log(docSnap.data())
                 setListing(docSnap.data())
                 setLoading(false)
             }
@@ -92,7 +94,17 @@ function Listing() {
 
                 <p className="listingLocationTitle">Location</p>
 
-                {/* MAP */}
+                <div className="leafletContainer">
+                    <MapContainer style={{height: '100%', width: '100%'}} center={[listing.geolocation.lat, listing.geolocation.lng]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer 
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                        />
+                        <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                            <Popup>{listing.location}</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
 
                 {auth.currentUser?.uid !== listing.userRef && (
                     <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className="primaryButton">
